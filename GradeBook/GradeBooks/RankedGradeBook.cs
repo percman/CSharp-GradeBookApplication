2c8df1580e3b4841ab8c96019c008c58ea93ad77
Daniel Abramson <daniel.abramson@ruralsource.com>
2018-09-06 09:40:57 -0600

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -26,5 +26,25 @@ namespace GradeBook.GradeBooks
             else if (grades[threshold * 4 - 1] <= averageGrade) return 'D';
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            else base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            else base.CalculateStudentStatistics(name);
+        }
     }
 }
